1d8251957ebe3a6d0684fdaac05c92ae4cfea970
Andre Popp <8mayday@gmail.com>
2026-09-18 15:03:28 +0200

    kido 0.7.1: search matches Claude session titles

--- Formula/kido.rb
+++ Formula/kido.rb
@@ -2,8 +2,8 @@ class Kido < Formula
   desc "Tmux sidebar for Claude Code sessions"
   homepage "https://github.com/andreypopp/kido"
   url "https://github.com/andreypopp/kido.git",
-      revision: "1bb37711e11f06d0e64f7f7f4e48f5f3068f03fd"
-  version "0.7.0"
+      revision: "01f0e6e4d45c62f49e27873fb32992f6908a86da"
+  version "0.7.1"
   head "https://github.com/andreypopp/kido.git", branch: "main"
 
   depends_on "go" => :build
