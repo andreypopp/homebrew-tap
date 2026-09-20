c190c9fe3f80dd2a29edb0f12528792a38a25bbe
Andre Popp <8mayday@gmail.com>
2026-09-20 13:18:02 +0200

    kido 0.7.6: dismissed prompts end at hook time

--- Formula/kido.rb
+++ Formula/kido.rb
@@ -2,8 +2,8 @@ class Kido < Formula
   desc "Tmux sidebar for Claude Code sessions"
   homepage "https://github.com/andreypopp/kido"
   url "https://github.com/andreypopp/kido.git",
-      revision: "a10aea397be463b393a6443a52948346f55612e7"
-  version "0.7.5"
+      revision: "4f139975afc43a5836f07278efa0a4fe18e1acff"
+  version "0.7.6"
   head "https://github.com/andreypopp/kido.git", branch: "main"
 
   depends_on "go" => :build
