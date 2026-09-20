45975895ba8bd7fa13eed96a32efccd5c052b5d2
Andre Popp <8mayday@gmail.com>
2026-09-20 13:04:25 +0200

    kido 0.7.5: hook debug log

--- Formula/kido.rb
+++ Formula/kido.rb
@@ -2,8 +2,8 @@ class Kido < Formula
   desc "Tmux sidebar for Claude Code sessions"
   homepage "https://github.com/andreypopp/kido"
   url "https://github.com/andreypopp/kido.git",
-      revision: "0f103ed215babb140c2971c2ded39ee243deff43"
-  version "0.7.4"
+      revision: "a10aea397be463b393a6443a52948346f55612e7"
+  version "0.7.5"
   head "https://github.com/andreypopp/kido.git", branch: "main"
 
   depends_on "go" => :build
