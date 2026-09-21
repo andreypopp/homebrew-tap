778413efba0028abaeb7039c50c5b2578c410009
Andre Popp <8mayday@gmail.com>
2026-09-21 17:06:50 +0200

    kido 0.18.1

--- Formula/kido.rb
+++ Formula/kido.rb
@@ -2,8 +2,8 @@ class Kido < Formula
   desc "Tmux sidebar for Claude Code sessions"
   homepage "https://github.com/andreypopp/kido"
   url "https://github.com/andreypopp/kido.git",
-      revision: "cb8d689826c758067bf9b2fd701b4ae475f5783d"
-  version "0.18.0"
+      revision: "dc456fff3b35bf891d63d4575873e2d0651c97b7"
+  version "0.18.1"
   head "https://github.com/andreypopp/kido.git", branch: "main"
 
   depends_on "go" => :build
