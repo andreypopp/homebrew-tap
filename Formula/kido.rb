c3f9b6af40e3f8260724324b5428c7db4d099648
Andre Popp <8mayday@gmail.com>
2026-09-21 09:02:59 +0200

    kido 0.10.3: search matches ssh destinations

--- Formula/kido.rb
+++ Formula/kido.rb
@@ -2,8 +2,8 @@ class Kido < Formula
   desc "Tmux sidebar for Claude Code sessions"
   homepage "https://github.com/andreypopp/kido"
   url "https://github.com/andreypopp/kido.git",
-      revision: "20a6feda426f9eed563033cff5414664098f29c4"
-  version "0.10.2"
+      revision: "5144e4e7248bc3c1a49a1527c4ecf099fb913fda"
+  version "0.10.3"
   head "https://github.com/andreypopp/kido.git", branch: "main"
 
   depends_on "go" => :build
