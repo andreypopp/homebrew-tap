a49711602abaddd24d4dcb9663916c4534db7c1a
Andre Popp <8mayday@gmail.com>
2026-09-25 16:01:52 +0200

    kido 0.26.4

--- Formula/kido.rb
+++ Formula/kido.rb
@@ -2,8 +2,8 @@ class Kido < Formula
   desc "Terminal multiplexer for coding agent sessions: tmux with a live side column"
   homepage "https://github.com/andreypopp/kido"
   url "https://github.com/andreypopp/kido.git",
-      revision: "d7e3d37ab4770d862d31580586fb33984df69180"
-  version "0.26.3"
+      revision: "fbdc805bc47bec6854a870f3e7eb895e80b09067"
+  version "0.26.4"
   head "https://github.com/andreypopp/kido.git", branch: "main"
 
   depends_on "autoconf" => :build
